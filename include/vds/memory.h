141f095d8f363c53fb2be9a154f3cfe5322e47c0
NicolasIDias <nicolasbhgamer@gmail.com>
2026-03-21 19:34:12 -0300

    feat: add arena function declarations

--- include/vds/memory.h
+++ include/vds/memory.h
@@ -7,3 +7,8 @@ typedef struct Arena {
     size_t size;
     size_t offset;
 } arena_t;
+
+arena_t vds_arena_init(size_t size);
+void *vds_alloc(size_t size, arena_t *arena);
+void vds_arena_reset(arena_t *arena);
+void vds_arena_destroy(arena_t *arena);
